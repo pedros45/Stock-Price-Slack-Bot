405adc93cb8d374047a707fd232e52c67be2f8cd
Jeremiah Pierson <jpierson4323@gmail.com>
2020-12-14 18:14:08 -0800

    Events

--- slackbot.py
+++ slackbot.py
@@ -32,7 +32,7 @@ from slackeventsapi import SlackEventAdapter
 #the slack API is going to send a post request to our web server.
 #From here we can get the request and handle it accordingly.
 #Take URL from ngrok and past it into REQUEST URL, append /slack/events
-#Mine looks like:  http://43d1cd57d0ae.ngrok.io/slack/events
+#Mine looks like:  http://43d1cd57d0ae.ngrok.io/slack/events   #NOTE THIS CHANGES EVERYTIME NGROK IS RESTARTED, UPDATE
 #Python code and ngrok should be running this time for slack API to verify url
 #Slack API website, Subscribe to bot events -> Add Bot User Event ->
 #enter: message.channels
@@ -49,9 +49,51 @@ app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(
     os.environ['SIGNING_SECRET'], '/slack/events', app)
 
-
 client = slack.WebClient(os.environ['TOKEN'])
-client.chat_postMessage(channel='#test', text="TestingBot")
+
+#if bot is sending message, dont respond. Only respond to users
+BOT_ID = client.api_call("auth.test")['user_id']
+
+
+def get_data(ticker):
+    ticker_data = yf.Ticker(ticker)
+
+    # ticker_info holds all the data from the company
+    # holds in a dictionary.
+    ticker_info = ticker_data.info
+    comp_name = ticker_info['shortName'] #for apple, gets "Apple Inc."
+    print(comp_name)
+
+    # get stock values for current day
+    todays_date = datetime.datetime.today().isoformat()
+    print('Today = ' + todays_date)
+
+    # get stock values for current certain time frame (this is month of oct)
+    # outputs first 10 characters from todays date
+    stock_data = ticker_data.history(period='1d', start='2020-10-01', end=todays_date[:10])
+
+    # get most current stock price from closing cost, get the last one from stockData
+    most_current_price = stock_data['Close'].iloc[-1]
+
+    # get last stock value to calculate change in value
+    price_last = stock_data['Close'].iloc[-2]
+    price_change = price_last - most_current_price
+
+    # output data (currentPrice is float value, cast to string)
+    print(comp_name + ' Last stock value = $' + str(most_current_price))
+    print('Change in stock value = ' + str(price_change))
+
+    return f"Name: {comp_name} \n Date: {todays_date} \n Current Price: {most_current_price} \n Price Change: {price_change} "
+
+@slack_event_adapter.on('message')
+def message(payload):  #when message is sent, take and handle payload
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+    if BOT_ID != user_id:
+        client.chat_postMessage(channel=channel_id, text=get_data(text))
 
 if __name__ == '__main__':
     #if file ran directly, run web server on default port 5000
@@ -59,66 +101,5 @@ if __name__ == '__main__':
     # Run through tickers and output data
     #for ticker in tickers_list:
         #send_message(get_data(ticker))
-
-
-# def get_data(ticker):
-#     ticker_data = yf.Ticker(ticker)
-#
-#     # ticker_info holds all the data from the company
-#     # holds in a dictionary.
-#     ticker_info = ticker_data.info
-#     comp_name = ticker_info['shortName'] #for apple, gets "Apple Inc."
-#     print(comp_name)
-#
-#     # get stock values for current day
-#     todays_date = datetime.datetime.today().isoformat()
-#     print('Today = ' + todays_date)
-#
-#     # get stock values for current certain time frame (this is month of oct)
-#     # outputs first 10 characters from todays date
-#     stock_data = ticker_data.history(period='1d', start='2020-10-01', end=todays_date[:10])
-#
-#     # get most current stock price from closing cost, get the last one from stockData
-#     most_current_price = stock_data['Close'].iloc[-1]
-#
-#     # get last stock value to calculate change in value
-#     price_last = stock_data['Close'].iloc[-2]
-#     price_change = price_last - most_current_price
-#
-#     # output data (currentPrice is float value, cast to string)
-#     print(comp_name + ' Last stock value = $' + str(most_current_price))
-#     print('Change in stock value = ' + str(price_change))
-#
-#     return f"Name: {comp_name} \n Date: {todays_date} \n Current Price: {most_current_price} \n Price Change: {price_change} "
-#
-#
-# def send_message(msg):
-#     client = slack.WebClient(os.environ['TOKEN'])
-#     BOT_ID = client.api_call("auth.text")["user_id"]    #returns ID of bot
-#
-#
-#     @slack_event_adapter.on('message')
-#     def message(payload):  #when message is sent, take and handle payload
-#         event = payload.get('event', {})
-#         channel_id = event.get('channel')
-#         user_id = event.get('user')
-#         text = event.get('text')
-#
-#         #determine if message being read is from user or from the bot
-#         if BOT_ID != user_id:
-#             client.chat_postMessage(channel=channel_id, text=text)
-#
-#     try:
-#         response = client.chat_postMessage(channel='#test', text="TestingBot")
-#     except SlackApiError as e:
-#         print(f"Got an error: {e.response['error']}")
-#
-#
-# if __name__ == '__main__':
-#     #if file ran directly, run web server on default port 5000
-#     app.run(debug=True)
-#     # Run through tickers and output data
-#     #for ticker in tickers_list:
-#         #send_message(get_data(ticker))
 
 
